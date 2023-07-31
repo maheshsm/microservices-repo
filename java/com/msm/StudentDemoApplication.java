faa83f5f370ccfbd5ba17890e63ecee6d6be054e
maheshsm <maheshsm07@gmail.com>
2023-07-31 21:07:02 +0530

    Initial commit (#1)

---
+++ java/com/msm/StudentDemoApplication.java
@@ -0,0 +1,15 @@
+package com.msm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDemoApplication.class, args);
+		
+		System.out.println("-------------------------------------- Application started successfully ----------------------------------------");
+	}
+
+}
